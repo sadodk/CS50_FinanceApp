faf9eb1d3f6046f22d237e8de22756ecd8a1d4f2
sadodk <56631862+sadodk@users.noreply.github.com>
2022-03-26 22:21:15 +0100

    Update application.py

--- application.py
+++ application.py
@@ -394,6 +394,7 @@ def sell():
     else:
         return render_template("sell.html", rows=rows)
 
-if __name__ == "__main__":
- port = int(os.environ.get("PORT", 8080))
- app.run(host="0.0.0.0", port=port)
+if __name__ == ‘__main__’:
+ app.debug = True
+ port = int(os.environ.get(“PORT”, 5000))
+ app.run(host=’0.0.0.0', port=port)
