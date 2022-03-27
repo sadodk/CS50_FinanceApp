d04861722a44f3f7573290a8936419fb3e049f1e
sadodk <56631862+sadodk@users.noreply.github.com>
2022-03-27 12:19:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -395,8 +395,3 @@ def sell():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("sell.html", rows=rows)
-
-if __name__ == ‘__main__’:
- app.debug = True
- port = int(os.environ.get(“PORT”, 5000))
- app.run(host=’0.0.0.0', port=port)
